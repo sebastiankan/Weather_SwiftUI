3f3873d66b177e4a7fa2676a931f7eb39396d428
Saba Kananian <kananian.saba@gmail.com>
2021-05-29 14:31:19 +0430

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Saba Kananian on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
